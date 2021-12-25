efca79d6f516754a4c8b08fb1bbddaa1214a5225
gilevro <alekseirusskih@gmail.com>
2021-12-25 21:16:26 +0700

    создание узла

--- linked_list.c
+++ linked_list.c
@@ -6,20 +6,39 @@
 /*   By: gilevro <alexeyrusskikh@protonmail.c       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/25 18:54:40 by gilevro           #+#    #+#             */
-/*   Updated: 2021/12/25 19:02:34 by gilevro          ###   ########.fr       */
+/*   Updated: 2021/12/25 20:15:16 by gilevro          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <inttypes.h>
+#include <stdlib.h>
+
+struct s_list
+{
+	int64_t			value;
+	struct s_list	*next;
+};
+
+void	list_print(const struct s_list *l)
+{
+	while (l)
+	{
+		printf("%" PRId64, l-> value);
+		l = l->next;
+	}
+}
+
+struct s_list	*node_create( int64_t value)
+{
+	struct s_list	*node;
+
+	node = malloc(sizeof(struct s_list));
+	node->value = value;
+	return (node);
+}
 
 int	main(void)
 {
-	struct list
-	{
-		int64_t value;
-		struct list* next;
-	};
-
-
+	return (0);
 }

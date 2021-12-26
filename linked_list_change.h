327127350000a81b9c27dc6469caf231118c0407
air2@tpu.ru <air2@tpu.ru>
2021-12-26 17:32:50 +0700

    реализована запись в конец списска

--- linked_list_change.h
+++ linked_list_change.h
@@ -6,7 +6,7 @@
 /*   By: gilevro <alexeyrusskikh@protonmail.c       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/26 15:18:37 by gilevro           #+#    #+#             */
-/*   Updated: 2021/12/26 16:58:34 by gilevro          ###   ########.fr       */
+/*   Updated: 2021/12/26 17:30:45 by gilevro          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef LINKED_LIST_CHANGE_H
@@ -24,6 +24,7 @@ struct s_list	*node_create( int64_t value)
 
 	node = malloc(sizeof(struct s_list));
 	node->value = value;
+	node->next = NULL;
 	return (node);
 }
 
@@ -47,9 +48,21 @@ void	list_destroy( struct s_list *list )
 		list = temp;
 	}
 }
-/*void	list_add_back( struct s_list** old, int64_t value )
+
+void	list_add_back(struct s_list **old, int64_t value)
 {
-	
+	struct s_list	*node;
+	struct s_list	*last;
+
+	if (*old != NULL)
+	{
+		node = node_create(value);
+		last = list_last(*old);
+		last->next = node;
+	}
+	else
+	{
+		list_add_front(old, value);
+	}
 }
-*/
 #endif
